1c10676b27382ff105c3f7b830aee60fa98a6350
yuveex <119145565+yuveex@users.noreply.github.com>
2025-07-15 09:15:44 +0000

    Deploying to gh-pages from @ yuveex/yuveex.github.io@c9d3dc88b483597f1d02c7399ef22735e7a63a75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-alvin-s-go",
-    title: "Alvin S. Go",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Feel free to reach out to me for more information!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
